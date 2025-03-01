57a1dc1519815a87fb461e0e9aed4d6760ba7247
rangurahul123 <23r95a7313@tkrec.ac.in>
2025-03-01 14:52:04 +0530

    Add files via upload

---
+++ rahul.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        
+        // Addition
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+}
